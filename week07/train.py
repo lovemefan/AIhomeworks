dabf82856d365a1f0383294d20a007a0666cdb7d
lovemefan <450489712@qq.com>
2020-11-19 20:53:26 +0800

    保存模型参数，加载模型参数

--- week07/train.py
+++ week07/train.py
@@ -8,7 +8,7 @@ import argparse
 from tqdm import trange, tqdm
 from week07.dataLoader import DataLoader
 from week07.model import Model
-
+from matplotlib import pyplot as plot
 
 def init_args():
     """初始化输入参数"""
@@ -32,6 +32,7 @@ if __name__ == '__main__':
     args = init_args()
 
     parameters = None
+    # 读取参数
     if args.parameters:
         with open('parameters.pkl', 'rb') as file_to_read:
             # 通过pickle的load函数读取data1.pkl中的对象，并赋值给data2
@@ -63,13 +64,15 @@ if __name__ == '__main__':
         train_data = (dataLoader.test_img[:1000], dataLoader.test_label[:1000])
         train_data_size = len(train_data[1])
         tmp = [model.predict_label(train_data[0][i]).argmax() == train_data[1][i] for i in range(train_data_size)]
-        train_accuracy = tmp.count(True) / len(tmp)
+        train_accuracy_tmp = tmp.count(True) / len(tmp)
+
 
         # 利用测试集检验准确率
         test_data = (dataLoader.test_img, dataLoader.test_label)
         test_data_size = len(test_data[1])
         tmp = [model.predict_label(test_data[0][i]).argmax() == test_data[1][i] for i in range(test_data_size)]
-        test_accuracy = tmp.count(True)/len(tmp)
+        test_accuracy_temp = tmp.count(True)/len(tmp)
+        test_accuracy.append(test_loss_temp)
 
         print(f"train_loss:{train_loss_temp}")
         print(f"test_loss:{test_loss_temp}")
@@ -80,8 +83,26 @@ if __name__ == '__main__':
         # 保存参数
         # 打开(或创建)一个名为parameters.pkl的文件，打开方式为二进制写入(参数‘wb’)
         with open("parameters.pkl", "wb") as  file_to_save:
-            # 通过pickle模块中的dump函数将data1保存到data1.pkl文件中。
+            # 通过pickle模块中的dump函数将data1保存到pkl文件中。
             # 第三个参数为序列化使用的协议版本，0：ASCII协议，所序列化的对象使用可打印的ASCII码表示；1：老式的二进制协议；2：2.3版本引入的新二进制协议，较以前的更高效；-1：使用当前版本支持的最高协议。其中协议0和1兼容老版本的python。protocol默认值为0。
             pickle.dump(model.parameters, file_to_save, -1)
+        with open("result.pkl", "wb") as  file_to_save:
+            # 通过pickle模块中的dump函数将data1保存到pkl文件中。
+            # 第三个参数为序列化使用的协议版本，0：ASCII协议，所序列化的对象使用可打印的ASCII码表示；1：老式的二进制协议；2：2.3版本引入的新二进制协议，较以前的更高效；-1：使用当前版本支持的最高协议。其中协议0和1兼容老版本的python。protocol默认值为0。
+            pickle.dump({'train_loss': train_loss, "test_loss": test_loss,
+                         "train_accuracy":train_accuracy, "test_accuracy": test_accuracy}, file_to_save, -1)
 
-
+    # loss
+    plot.plot(range(len(train_loss)), train_loss, label='train_loss')
+    plot.plot(range(len(test_loss)), test_loss, label='test_loss')
+    plot.legend()  # 让图例生效
+    plot.xlabel('batch')  # X轴标签
+    plot.ylabel("loss")  # Y轴标签
+    plot.savefig('loss.png')
+    # accuracy
+    plot.plot(range(len(train_accuracy)), train_accuracy, label='train_accuracy')
+    plot.plot(range(len(test_accuracy)), test_accuracy, label='test_accuracy')
+    plot.legend()  # 让图例生效
+    plot.xlabel('batch')  # X轴标签
+    plot.ylabel("accuracy")  # Y轴标签
+    plot.savefig('accuracy.png')
